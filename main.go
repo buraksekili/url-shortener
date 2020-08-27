5ecfa0ee8516782ea6c826c630d532c6868da9bd
buraksekili <buraksekili@gmail.com>
2020-08-27 21:26:16 +0300

    Changed function of shortener

--- main.go
+++ main.go
@@ -3,18 +3,20 @@ package main
 import (
 	"./shortener"
 	"fmt"
+	"github.com/fatih/color"
 	"net/http"
 )
 
 func main() {
-	urls, err := shortener.InitPathStruct()
+
+	url, err := shortener.CheckOp()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	urlHandler := shortener.URLHandler(urls, http.NewServeMux())
+	urlHandler := shortener.URLHandler(url, http.NewServeMux())
 	fmt.Println("Live on port: ", 3000)
 	if err := http.ListenAndServe(":3000", urlHandler); err != nil {
-		fmt.Println("error:", err)
+		fmt.Printf("%s %s\n", color.RedString("Error:"), err)
 	}
 }
